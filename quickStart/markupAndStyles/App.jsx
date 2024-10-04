412c433cb67c4c66bfa2ca622045bac079501f0f
Itallo Vasconcelos <itallo.vasconcelos@versatushpc.com.br>
2024-10-04 17:01:58 -0300

    Adding CSS and JSX Markup

---
+++ quickStart/markupAndStyles/App.jsx
@@ -0,0 +1,13 @@
+function AboutPage() {
+    return (
+        <>
+        <h1>About</h1>
+            <p>Hello there. <br />
+            How do you do?
+            </p>
+        </>
+    );
+}
+//To specify CSS class, use 'className':
+<img className="example" />
+
